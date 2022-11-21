9a97b7625360635e81b8673fce70ead9e0a4719e
MercyWain <118727117+MercyWain@users.noreply.github.com>
2022-11-21 14:10:51 +0300

    Add files via upload

---
+++ Sample Queries.sql
@@ -0,0 +1,14 @@
+SELECT * FROM public."Ingredients"
+ORDER BY "Ingredient_ID" ASC 
+
+SELECT * FROM public."Pizza" WHERE "Price" = '22'
+ORDER BY "Pizza_ID" ASC 
+
+SELECT * FROM public."Sales" WHERE "Quantity Sold" = '10'
+ORDER BY "Date" DESC
+
+SELECT Name FROM public."Server"
+ORDER BY "Server_ID" ASC 
+
+SELECT Address FROM public."Supplier"
+ORDER BY "Name" DESC  
